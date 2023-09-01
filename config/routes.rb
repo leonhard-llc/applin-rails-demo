60ee7c286c913b70010a9db2149c24fc82574a86
Michael Leonhard <michael@leonhardllc.com>
2023-09-01 10:29:22 -0700

    Configure render to use free plan. Add healthcheck.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # https://guides.rubyonrails.org/routing.html
   root "home#index" # Requests for "/" go to HomeController.index.
+  get '/healthz', to: proc { [200, {}, ['success']] }
 end
