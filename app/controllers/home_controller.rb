596b1f7c4d6631e2580baae8461ce97491891929
Michael Leonhard <michael@leonhardllc.com>
2023-09-04 13:20:40 -0700

    Replace count demo with full demo.  Add Pages and Widgets sections.  Still needs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,60 @@
 class HomeController < ApplicationController
   include ::Applin::Rails
-  @@count = 0
 
+  #   def NNNNNNN
+  #     send_page nav_page(title: "NNNNN") {
+  #       scroll {
+  #         column(widgets: [
+  #           form_section(title: "NNNNN", widgets: [
+  #             nav_button(text: "NNNNNN", actions: [push("/NNNNN")]),
+  #           ]),
+  #         ])
+  #       }
+  #     }
+  #   end
   def index
-    send_page plain_page {
+    send_page nav_page(title: "Applin Demo", poll_seconds: 30) {
       scroll {
         column(widgets: [
-          form_section(widgets: [
-            text("Hello World!"),
-            text("Page views since server start: #{self.next_count}"),
-            button(text: "Poll", actions: [poll])
-          ])
+          form_section(title: "Pages", widgets: [
+            nav_button(text: "Alert Modal", actions: [push("/alert_modal_page")]),
+            nav_button(text: "Drawer Modal", actions: [push("/draw_modal_page")]),
+            nav_button(text: "Nav Page", actions: [push("/nav_page_page")]),
+            nav_button(text: "Plain Page", actions: [push("/plain_page_page")]),
+          ]),
+          form_section(title: "Widgets", widgets: [
+            nav_button(text: "Back Button", actions: [push("/back_button_page")]),
+            nav_button(text: "Button", actions: [push("/button_page")]),
+            nav_button(text: "Checkbox", actions: [push("/checkbox_page")]),
+            nav_button(text: "Error Text", actions: [push("/error_text_page")]),
+            nav_button(text: "Form Button", actions: [push("/form_button_page")]),
+            nav_button(text: "Form Section", actions: [push("/form_section_page")]),
+            nav_button(text: "Grouped Row Table", actions: [push("/grouped_row_table_page")]),
+            nav_button(text: "Image", actions: [push("/image_page")]),
+            nav_button(text: "Nav Button", actions: [push("/nav_button_page")]),
+            nav_button(text: "Text", actions: [push("/text_page")]),
+            nav_button(text: "Textfield", actions: [push("/textfield_page")]),
+          ]),
+          # form_section(title: "Photos", widgets: [
+          #   nav_button(text: "Choose Photo", actions: [push("/upload-photo")]),
+          #   nav_button(text: "Take Photo", actions: [push("/take-photo")]),
+          #   nav_button(text: "View Photo", actions: [push("/view_photo_page")]),
+          # ]),
+          # form_section(title: "Update Modes", widgets: [
+          #   nav_button(text: "Inert", actions: [push("/inert_page")]),
+          #   nav_button(text: "Poll", actions: [push("/poll_page")]),
+          # # TODO: Implement streaming page updates.
+          # # nav_button(text: "stream_page", actions: [push("/stream_page")]),
+          # ]),
+          # form_section(title: "Vars", widgets: [
+          #   nav_button(text: "Check Vars", actions: [push("/check_vars_page")]),
+          # ]),
         ])
       }
     }
   end
 
-  private
-
-  def next_count
-    @@count += 1
+  def button_pressed_page
+    send_page alert_modal(title: "Button Pressed", modal_buttons: [ok_modal_button])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,32 @@
 Rails.application.routes.draw do
   # https://guides.rubyonrails.org/routing.html
+  get "/healthz", to: proc { [200, {}, ["success"]] }
+  post "/ok", to: proc { [200, {}, ["success"]] }
+  post "/error", to: proc { [400, {}, ["error1"]] }
+
   root "home#index" # Requests for "/" go to HomeController.index.
-  get '/healthz', to: proc { [200, {}, ['success']] }
+  get "/button_pressed_page", to: "home#button_pressed_page"
+
+  get "/alert_modal_page", to: "pages#alert_modal_page"
+  get "/draw_modal_page", to: "pages#draw_modal_page"
+  get "/nav_page_page", to: "pages#nav_page_page"
+  get "/plain_page_page", to: "pages#plain_page_page"
+
+  get "/back_button_default_page", to: "widgets#back_button_default_page"
+  get "/back_button_disabled_page", to: "widgets#back_button_disabled_page"
+  get "/back_button_missing_page", to: "widgets#back_button_missing_page"
+  get "/back_button_rpc_ok_page", to: "widgets#back_button_rpc_ok_page"
+  get "/back_button_rpc_error_page", to: "widgets#back_button_rpc_error_page"
+  get "/back_button_page", to: "widgets#back_button_page"
+  get "/button_page", to: "widgets#button_page"
+  get "/checkbox_page", to: "widgets#checkbox_page"
+  get "/error_text_page", to: "widgets#error_text_page"
+  get "/form_button_page", to: "widgets#form_button_page"
+  get "/form_section_page", to: "widgets#form_section_page"
+  get "/grouped_row_table_page", to: "widgets#grouped_row_table_page"
+  get "/image_page", to: "widgets#image_page"
+  get "/nav_button_page", to: "widgets#nav_button_page"
+  get "/textfield_page", to: "widgets#textfield_page"
+  get "/text_page", to: "widgets#text_page"
+  # get "/NNNNN", to: "widgets#NNNNNN"
 end
