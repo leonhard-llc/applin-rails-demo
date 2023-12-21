bb0d9f676223ce4fe82ec6d835d41f1bdab28f92
Michael Leonhard <michael@leonhardllc.com>
2023-12-20 16:14:54 -0800

    Show error on username field.

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -3,13 +3,30 @@ require "applin"
 class AccountController < ApplicationController
   protect_from_forgery with: :exception, if: -> { !request.format.applin? }
 
+  def agree_param
+    params[:agree] == true
+  end
+
+  def username_param
+    value = self.params[:username].to_s.strip.downcase
+    if /^[a-z0-9]+$/.match?(value)
+      value
+    end
+  end
+
+  def new_account
+    # These values are available to templates.
+    @agree = self.agree_param
+    @username_bad = self.params[:username] && self.username_param.nil?
+  end
+
   def create_account
-    if !params[:agree]
+    if !self.agree_param
       return render plain: "You must agree to the terms",
                     status: :unprocessable_entity
     end
-    username = params[:username].to_s.downcase.strip
-    if !/^[a-z0-9]+$/.match?(username)
+    username = self.username_param
+    if username.nil?
       return render plain: "Please enter letters and numbers",
                     status: :unprocessable_entity
     end
--- app/views/account/new_account.applin.jbuilder
+++ app/views/account/new_account.applin.jbuilder
@@ -1,13 +1,21 @@
 json.page Applin::nav_page(title: "New Account") {
   Applin::scroll {
     Applin::form(widgets: [
-      Applin::textfield(label: "Username", var_name: "username"),
+      Applin::textfield(
+        var_name: "username",
+        label: "Username",
+        error: @username_bad ? "Please enter letters and numbers" : nil,
+      ),
       Applin::nav_button(text: "Terms", actions: [Applin::push("/terms")]),
       Applin::nav_button(text: "Privacy", actions: [Applin::push("/privacy")]),
       Applin::checkbox(text: "I agree", var_name: "agree"),
       Applin::form_button(
         text: "Create Account",
-        actions: [Applin::rpc("/create_account"), Applin::replace_all("/")],
+        actions: [
+          Applin::rpc("/create_account"),
+          Applin::on_user_error_poll,
+          Applin::replace_all("/"),
+        ],
       ),
     ])
   }
